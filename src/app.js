04c18d18d637954a3778950796b03501abfddca9
noheliabam <41593301+noheliabam@users.noreply.github.com>
2025-05-20 22:57:29 +0000

    Generador de Excusas ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  const who   = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action= ['ate',       'peed on',   'crushed',     'broke'];
+  const what  = ['my homework','my phone', 'the car'];
+  const when  = ['before the class','when I was sleeping',
+                 'while I was exercising','during my lunch',
+                 'while I was praying'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = `${who[rand(who)]} ${action[rand(action)]} ${what[rand(what)]} ${when[rand(when)]}`;
+  document.querySelector('#excuse').innerText = excuse;
 };
+
+function rand(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
