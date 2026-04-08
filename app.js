41847380befb2c5fbd03b660716b62bcd4366ce1
noheliabam <41593301+noheliabam@users.noreply.github.com>
2026-04-08 19:05:29 +0000

    cambios css

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
-window.onload = () => {
-  const who = [
-    'An alien',
-    'My imaginary friend',
-    'The neighbor’s chicken',
-    'A wizard',
-    'A talking potato'
-  ];
-  const action = [
-    'stole',
-    'burned',
-    'hid',
-    'chewed on',
-    'deleted'
-  ];
-  const what = [
-    'my project',
-    'the laptop charger',
-    'my presentation',
-    'the report',
-    'my schedule'
-  ];
-  const when = [
-    'right before class',
-    'during breakfast',
-    'while I was brushing my teeth',
-    'as I was leaving the house',
-    'when I opened the door'
-  ];
+document.addEventListener('DOMContentLoaded', () => {
+  const boton = document.querySelector('#generate');
 
+  boton.addEventListener('click', generarExcusa);
+
+  generarExcusa();
+});
+
+const who = [
+  'An alien',
+  'My imaginary friend',
+  'The neighbor’s chicken',
+  'A wizard',
+  'A talking potato'
+];
+
+const action = [
+  'stole',
+  'burned',
+  'hid',
+  'chewed on',
+  'deleted'
+];
+
+const what = [
+  'my project',
+  'the laptop charger',
+  'my presentation',
+  'the report',
+  'my schedule'
+];
+
+const when = [
+  'right before class',
+  'during breakfast',
+  'while I was brushing my teeth',
+  'as I was leaving the house',
+  'when I opened the door'
+];
+
+function generarExcusa() {
   const excuse = `${who[rand(who)]} ${action[rand(action)]} ${what[rand(what)]} ${when[rand(when)]}.`;
+
   document.querySelector('#excuse').innerText = excuse;
-};
+}
 
 function rand(arr) {
   return Math.floor(Math.random() * arr.length);
 }
-
